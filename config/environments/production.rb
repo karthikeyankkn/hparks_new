689984ea9bcdc078aefaeb9ec0cd64304dfe3cfc
colkar99 <colkar99@gmail.com>
2018-05-15 11:49:58 +0530

    asas

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "http://hparks.getspini.in/",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "noreplay.hparks@gmail.com",
+    password: "hparks.9"
+
+  }
+  config.action_mailer.default_url_options = { host: "http://hparks.getspini.in/"}
 end
