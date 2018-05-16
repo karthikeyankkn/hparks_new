05cf82685ac5f08b58e5b8a8c2a04c811368018d
colkar99 <colkar99@gmail.com>
2018-05-16 13:42:46 +0530

    asas

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rvm_ruby_version, '2.4.1'
 set :rvm_type, :system
 set :passenger_environment_variables, { :path => '/path-to-passenger/bin:$PATH' }
-# set :passenger_restart_command, '/path-to-passenger/bin/passenger-config restart-app'
+set :passenger_restart_command, '/path-to-passenger/bin/passenger-config restart-app'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
