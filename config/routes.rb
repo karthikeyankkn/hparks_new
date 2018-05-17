82bb4ace863f5c00a56c8d7a422c35d8bc013841
colkar99 <colkar99@gmail.com>
2018-05-17 17:08:05 +0530

    nav bar link pages created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  resources :homes
 	# root :to => "payments#new"
-	root :to => "user_details#new"
+	# root :to => "user_details#new" use this for paymment gateway
+	root :to => 'homes#index'
+	get "about-us",to: "homes#aboutus"
+	get "about-oragadam",to: "homes#about_oragadam"
+	get "plots-oragadam",to: "homes#plots_oragadam"
+	get "apartment", to: "homes#apartment"
+	get "amenities",to: "homes#amenities"
+	get "rental", to: "homes#rental"
+	get "commercial-retail",to: "homes#commercial_retail"
+	get "contact-us", to: "homes#contact_us"
 	post "create",to:  "user_details#create"
 
 	# post "transaction/ccavRequestHandler",to: "payments#requestHandler"
