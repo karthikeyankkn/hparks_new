dbbc0938b00c053f3cdf4219081f8acb21132134
colkar99 <colkar99@gmail.com>
2018-05-21 12:36:15 +0530

    new changes

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -94,4 +93,3 @@ ActiveRecord::Schema.define(version: 2018_05_18_095732) do
   end
 
 end
-
