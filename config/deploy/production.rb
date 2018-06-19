c22c96b60ae3b007fab2ecde1b0c4ffbd762b6fb
colkar99 <colkar99@gmail.com>
2018-06-19 13:47:00 +0530

    asas

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -61,23 +61,23 @@
 #   }
 
 ##production code ################hiranandaniparks.com
-role :app, %w{ubuntu@13.126.86.237}
-role :web, %w{ubuntu@13.126.86.237}
-role :db,  %w{ubuntu@13.126.86.237}
-set :ssh_options, {
-  keys: %w(~/.ssh/hiranandani.pem),
-  forward_agent: false,
-  auth_methods: %w(publickey password)
-}
+# role :app, %w{ubuntu@13.126.86.237}
+# role :web, %w{ubuntu@13.126.86.237}
+# role :db,  %w{ubuntu@13.126.86.237}
+# set :ssh_options, {
+#   keys: %w(~/.ssh/hiranandani.pem),
+#   forward_agent: false,
+#   auth_methods: %w(publickey password)
+# }
 
 
 
 ##########staging code############hparks.getspini.in
-# role :app, %w{ubuntu@54.251.179.157}
-# role :web, %w{ubuntu@54.251.179.157}
-# role :db,  %w{ubuntu@54.251.179.157}
-# set :ssh_options, {
-#   keys: %w(~/.ssh/kp_1.pem),
-#   forward_agent: false,
-#   auth_methods: %w(publickey password)
-# }
+role :app, %w{ubuntu@54.251.179.157}
+role :web, %w{ubuntu@54.251.179.157}
+role :db,  %w{ubuntu@54.251.179.157}
+set :ssh_options, {
+  keys: %w(~/.ssh/kp_1.pem),
+  forward_agent: false,
+  auth_methods: %w(publickey password)
+}
