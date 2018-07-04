ba353efba607b7fc4a229ee377b465fc85056416
karthikeyankkn <connecttokkn@hotmail.com>
2018-07-04 16:25:58 +0530

    added dynamic image change and hided mails for others

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -18,7 +18,9 @@ class PaymentMailer < ApplicationMailer
   end
   def lead_post(user)
   	 @user = user
-  	 mail to: "<karthikeyan@spinircle.com>,<joshua.simon@spinircle.com>,<contact@getspini.com>,<sprasad@hrealty.com>,<pvijay@hrealty.com>" , subject: "Lead response"
+  	 # mail to: "<karthikeyan@spinircle.com>,<joshua.simon@spinircle.com>,<contact@getspini.com>,<sprasad@hrealty.com>,<pvijay@hrealty.com>" , subject: "Lead response"
+     mail to: "<karthikeyan@spinircle.com>" , subject: "Lead response"
+
   end
   def payment_response_confirmation(user,payments)
   	 @user = user
