b646ecc90c09d2850b1a8a5ba5600dc210a07bab
karthikeyankkn <connecttokkn@hotmail.com>
2018-07-19 16:46:04 +0530

    modified deliver_later to now in mailers

--- app/controllers/user_details_controller.rb
+++ app/controllers/user_details_controller.rb
@@ -29,8 +29,8 @@ def create
 		# PaymentMailer.lead_post(@user_detail).deliver_later
 		# redirect_to plot_booking_confirmation_success_path(@user_detail)
 		############# end cuurent prodution down #############################
-		PaymentMailer.lead_registration(@user_detail).deliver_later
-		PaymentMailer.lead_post(@user_detail).deliver_later
+		PaymentMailer.lead_registration(@user_detail).deliver_now
+		PaymentMailer.lead_post(@user_detail).deliver_now
 		redirect_to plot_booking_thank_you_path(@user_detail)
 		##################################################
 		# redirect_to controller: 'payments', action: 'requestHandler', order_id: @user_detail[:id] 
