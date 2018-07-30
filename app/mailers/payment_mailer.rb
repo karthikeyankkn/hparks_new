e832025a6f05fc6a711d13765348ee64c1398146
karthikeyankkn <connecttokkn@hotmail.com>
2018-07-30 16:41:13 +0530

    added durairaj mail id in linkedin form

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -44,7 +44,7 @@ class PaymentMailer < ApplicationMailer
   end
   def spini_linkdin_leads(lead_details)
      @lead_details = lead_details
-     # mail to: "<karthikeyan@spinircle.com>,<joshua.simon@spinircle.com>,<anuradha.v@spinircle.com>,<sathesh@spinircle.com>,<anand.kumar@spinircle.com>" , subject: "Spini Campaign Leads"
-      mail to: "<karthikeyan@spinircle.com>" , subject: "Lead from Linkdin Campaign"
+     mail to: "<karthikeyan@spinircle.com>,<joshua.simon@spinircle.com>,<sathesh@spinircle.com>,<durairaju@webboombaa.com>,<contact@getspini.com>" , subject: "Linkedin Campaign Leads"
+      # mail to: "<karthikeyan@spinircle.com>" , subject: "Lead from Linkdin Campaign"
   end
 end
