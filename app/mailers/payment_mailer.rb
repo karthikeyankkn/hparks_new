0206df8d525493c33a9e5abdcf85ffa1f8d8a054
karthikeyankkn <connecttokkn@hotmail.com>
2018-11-19 15:48:18 +0530

    validation added for chennai and singapore campaign

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -18,13 +18,13 @@ class PaymentMailer < ApplicationMailer
   end
   def lead_post(user)
   	 @user = user
-  	 mail to: "<karthikeyan@spinircle.com>,<joshua.simon@spinircle.com>,<contact@getspini.com>,<sprasad@hrealty.com>,<pvijay@hrealty.com>" , subject: "Lead response"
+  	 mail to: "<karthikeyan@spinircle.com>,<joshua.simon@spinircle.com>,<contact@getspini.com>,<csekhar@hrealty.com>,<sprasad@hrealty.com>,<pvijay@hrealty.com>" , subject: "Lead response"
      # mail to: "<karthikeyan@spinircle.com>" , subject: "Lead response"
 
   end
   def site_visit_mail(user)
     @user = user
-     mail to: "<karthikeyan@spinircle.com>,<joshua.simon@spinircle.com>,<contact@getspini.com>,<sprasad@hrealty.com>,<pvijay@hrealty.com>" , subject: "Site vist response"
+     mail to: "<karthikeyan@spinircle.com>,<joshua.simon@spinircle.com>,<contact@getspini.com>,<csekhar@hrealty.com>,<sprasad@hrealty.com>,<pvijay@hrealty.com>" , subject: "Site vist response"
      # mail to: "<karthikeyan@spinircle.com>" , subject: "Site vist response"
   end
   def payment_response_confirmation(user,payments)
@@ -39,8 +39,13 @@ class PaymentMailer < ApplicationMailer
   end
   def spini_camp_leads(lead_details)
      @lead_details = lead_details
-     mail to: "<karthikeyan@spinircle.com>,<joshua.simon@spinircle.com>,<anuradha.v@spinircle.com>,<sathesh@spinircle.com>,<anand.kumar@spinircle.com>,<contact@getspini.com>,<sprasad@hrealty.com>,<Pvijay@hrealty.com>, <Indiamediasem@gmail.com>, <Praveentkmenon@gmail.com>" , subject: @lead_details.source
-     # mail to: "<karthikeyan@spinircle.com>" , subject: @lead_details.source
+     if lead_details.source == "Hiranandani Parks - CHN-Campaign-IND" && lead_details.source == "Hiranandani Parks - Sicci Campaign"
+        mail to: "<karthikeyan@spinircle.com>,<joshua.simon@spinircle.com>,<anuradha.v@spinircle.com>,<contact@getspini.com>,<csekhar@hrealty.com>,<sprasad@hrealty.com>,<Pvijay@hrealty.com>" , subject: @lead_details.source
+       else
+        mail to: "<karthikeyan@spinircle.com>,<joshua.simon@spinircle.com>,<anuradha.v@spinircle.com>,<sathesh@spinircle.com>,<anand.kumar@spinircle.com>,<contact@getspini.com>,<csekhar@hrealty.com>,<sprasad@hrealty.com>,<Pvijay@hrealty.com>, <Indiamediasem@gmail.com>, <Praveentkmenon@gmail.com>" , subject: @lead_details.source
+        # mail to: "<karthikeyan@spinircle.com>" , subject: @lead_details.source
+     end
+     
   end
   def spini_enquire_leads(lead_details)
      @lead_details = lead_details
@@ -49,7 +54,7 @@ class PaymentMailer < ApplicationMailer
   end
   def spini_linkdin_leads(lead_details)
      @lead_details = lead_details
-     mail to: "<karthikeyan@spinircle.com>,<joshua.simon@spinircle.com>,<sathesh@spinircle.com>,<tech@webboombaa.com>,<contact@getspini.com>" , subject: "Linkedin Campaign Leads"
+     mail to: "<karthikeyan@spinircle.com>,<joshua.simon@spinircle.com>,<sathesh@spinircle.com>,<tech@webboombaa.com>,<contact@getspini.com>,<csekhar@hrealty.com>" , subject: "Linkedin Campaign Leads"
       # mail to: "<karthikeyan@spinircle.com>" , subject: "Lead from Linkdin Campaign"
   end
 end
